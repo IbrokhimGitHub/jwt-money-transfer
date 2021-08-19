5bad7b8965d0882a878e1794be3eed062621b44f
IbrokhimGitHub <i.irmukhamedov@gmail.com>
2021-08-19 22:01:15 +0500

    firstCommit

--- src/main/java/uz/pdp/jwtmoneytransfer/config/SecurityConfig.java
+++ src/main/java/uz/pdp/jwtmoneytransfer/config/SecurityConfig.java
@@ -2,27 +2,35 @@ package uz.pdp.jwtmoneytransfer.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import uz.pdp.jwtmoneytransfer.securtity.JwtFilter;
 import uz.pdp.jwtmoneytransfer.service.AuthorizationService;
-
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     AuthorizationService authorizationService;
 
     @Autowired
     JwtFilter jwtFilter;
+    @Bean
+    PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/","/api/auth/login/").permitAll()
+                .antMatchers("/", "/api/auth/login/").permitAll()
                 .anyRequest().authenticated();
 
     }
@@ -32,13 +40,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .userDetailsService(authorizationService);
     }
-    @Bean
-   PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+
     @Bean
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
     }
+
+
 }
+
--- src/main/java/uz/pdp/jwtmoneytransfer/controller/LoginController.java
+++ src/main/java/uz/pdp/jwtmoneytransfer/controller/LoginController.java
@@ -2,9 +2,15 @@ package uz.pdp.jwtmoneytransfer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import uz.pdp.jwtmoneytransfer.payload.LoginDto;
 import uz.pdp.jwtmoneytransfer.securtity.JwtProvider;
 import uz.pdp.jwtmoneytransfer.service.AuthorizationService;
 
@@ -21,10 +27,20 @@ public class LoginController {
     JwtProvider jwtProvider;
     @Autowired
     AuthenticationManager authenticatidonManager;
-    @PostMapping("login")
-    public HttpEntity<?> loginToSystem(@RequestBody ){
-        try{
 
+    @PostMapping("login")
+    public HttpEntity<?> loginToSystem(@RequestBody LoginDto loginDto) {
+        try {
+            Authentication authenticate = authenticatidonManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    loginDto.getUsername(),
+                    loginDto.getPassword()
+            ));
+            String token = jwtProvider.tokenGenerator(loginDto.getUsername());
+            System.out.println(authenticate);
+            System.out.println(token);
+            return ResponseEntity.ok(token);
+        }catch (BadCredentialsException e){
+            return ResponseEntity.status(401).body("went wrong");
         }
     }
 }
